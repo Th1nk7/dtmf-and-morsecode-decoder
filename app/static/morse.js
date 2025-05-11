d06f64673f68a87b92d65f2152c4887cd3e58436
Th1nk <Tobiasransborg@hotmail.com>
2025-05-11 02:11:25 +0200

    stylingdone for morse

--- app/static/morse.js
+++ app/static/morse.js
@@ -1,6 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const morseVideo = document.getElementById("morseBarDisplay");
-    morseVideo.oncanplay = function() {
-      document.getElementById('loadingDisplay').hidden = true;
-    };
+const vid = document.getElementById('morseBarDisplay');
+
+function copyToClipboard() {
+  navigator.clipboard.writeText(document.getElementById('morseOutput').value ).then(function() {
+    console.log('Text copied to clipboard');
+  }, function(err) {
+    console.error('Could not copy text: ', err);
   });
+}
+
+function togglePlay() {
+  vid.paused ? vid.play() : vid.pause();
+}
+
+function toggleMute() {
+  if (vid.muted) {
+    vid.muted = false;
+    document.getElementById('volumeButton').innerText = 'volume_up';
+  } else {
+    vid.muted = true;
+    document.getElementById('volumeButton').innerText = 'volume_off';
+  }
+}
+
+function setVolume(value) {
+  vid.volume = value; // Set the video volume
+  if (value == 0) {
+    document.getElementById('volumeButton').innerText = 'volume_off';
+  } else {
+    document.getElementById('volumeButton').innerText = 'volume_up';
+  }
+}
+
+function stopVideo() {
+  vid.currentTime = 0;
+  vid.pause()
+}
